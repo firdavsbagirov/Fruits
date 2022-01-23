8b43726596fb2e881b1d4a1b3a2f7e6293f8ca72
Firdavs Bagirov <firdavsbagirov@abdigital.uz>
2022-01-23 18:18:29 +0500

    Initial Commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by Firdavs Bagirov on 23/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Firdavs Bagirov on 23/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
